91309c79a9e001f1e40fe0b7bb5ae4225fec9c64
00._lxh <lxcy.armendarizz@gmail.com>
2026-02-03 19:13:20 -0700

    Update versions.lua

--- versions.lua
+++ versions.lua
@@ -1,7 +1,7 @@
 local versions = {
 
 	["luna_hub"] = "v6.2.5";
-	["rakoof"] = "v2.3.2";
+	["rakoof"] = "v2.3.1";
 
 	["rake_remastered"] = "v1.2.3";
 	["revenant_sunrisen"] = "v2.3.3";
@@ -9,4 +9,5 @@ local versions = {
 
 
 return versions;
+
 
