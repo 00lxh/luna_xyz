68e87f8670484706c93fc2932b50910abae03fad
00._lxh <lxcy.armendarizz@gmail.com>
2026-04-10 10:43:23 -0600

    Update HookService.lua

--- src/utils/HookService.lua
+++ src/utils/HookService.lua
@@ -1,19 +1,25 @@
 local HookingService, hookedFunctions, signalFunctions = {}, {}, {};
-HookingService.ClassName = "HookingService"
+HookingService.ClassName = "HookingService";
+
+local newcclosure = (newcclosure or function(...) return ... end);
 
 function HookingService:Hook(__callback, __callback_hook)
+	
+	if not hookfunction then return function() return; end; end;
 
 	local hooked_function = hookfunction(__callback, newcclosure(function(...)
 		return __callback_hook(...);
 	end));
-	
-	hookedFunctions[__callback] = hooked_function;
 
+	hookedFunctions[__callback] = hooked_function;
 	return hooked_function;
 end;
 
 function HookingService:HookRemote(Object, __callback)
 
+	if not hookmetamethod then return function() return; end; end;
+	if not getnamecallmethod then return function() return; end; end;
+	
 	__callback = __callback or function() return nil; end;
 
 	local Old_Namecall; Old_Namecall = hookmetamethod(game, "__namecall", newcclosure(function(self, ...)
@@ -31,6 +37,9 @@ end;
 
 function HookingService:NameCallHook(Object, Method, __callback)
 
+	if not hookmetamethod then return function() return; end; end;
+	if not getnamecallmethod then return function() return; end; end;
+	
 	__callback = __callback or function() return nil; end;
 
 	local Old_Namecall; Old_Namecall = hookmetamethod(game, "__namecall", newcclosure(function(self, ...)
@@ -47,6 +56,8 @@ function HookingService:NameCallHook(Object, Method, __callback)
 end;
 
 function HookingService:IndexHook(Object, Property, Value)
+	
+	if not hookmetamethod then return function() return; end; end;
 
 	local Old_newIndex; Old_newIndex = hookmetamethod(game, "__newindex", newcclosure(function(self, __k, ...)
 
@@ -68,26 +79,33 @@ function HookingService:IndexHook(Object, Property, Value)
 
 	hookedFunctions[Old_newIndex] = { __hook = "__newindex"; __old = Old_newIndex; };
 	hookedFunctions[Old_Index] = { __hook = "__index"; __old = Old_Index; };
+	
+	return Old_newIndex, Old_Index;
 end;
 
 function HookingService:DisableConnection(Signal)
+	
+	if not getconnections then return; end;
 
-	table.insert(signalFunctions, Signal);
 	for _, v in next, getconnections(Signal) do v:Disable(); end;
+	table.insert(signalFunctions, Signal);
 end;
 
 function HookingService:Destroy()
 
 	for i, v in pairs(hookedFunctions) do
-		if typeof(i) == "function" and typeof(v) == "function" and ishooked(i) then restorefunction(i); end;
-		if typeof(i) == "function" and typeof(v) == "table" then hookmetamethod(game, v.__hook, v.__old); end;
+		if ishooked and restorefunction and typeof(i) == "function" and typeof(v) == "function" and ishooked(i) then restorefunction(i); end;
+		if hookmetamethod and typeof(i) == "function" and typeof(v) == "table" then hookmetamethod(game, v.__hook, v.__old); end;
 	end;
 
+	table.clear(hookedFunctions);
+	if not getconnections then return; end;
+	
 	for _, v in pairs(signalFunctions) do
 		for _, b in next, getconnections(v) do b:Enable(); end;
 	end;
-
-	table.clear(hookedFunctions); table.clear(signalFunctions);
+	
+	table.clear(signalFunctions);
 end;
 
 return HookingService;
