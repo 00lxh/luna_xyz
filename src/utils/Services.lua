f37948f88c1f2c7b27a122d282b1a79402d7f9d6
00._lxh <lxcy.armendarizz@gmail.com>
2026-04-11 22:21:24 -0600

    Update Services.lua

--- src/utils/Services.lua
+++ src/utils/Services.lua
@@ -1,15 +1,18 @@
 getgenv().Services = {};
 local cloneref = (cloneref or function(instance: any) return instance end);
 
-local luna_storage = Instance.new("Folder", gethui and gethui() or cloneref(game:GetService("CoreGui")));
-luna_storage.Name = "luna_storage";
+local coreGui = cloneref(game:GetService("CoreGui"));
+local gethui = (gethui or function() return coreGui; end);
+
+local luna_cache = Instance.new("Folder", gethui and gethui() or cloneref(game:GetService("CoreGui")));
+luna_cache.Name = "luna_cache";
 
 function Services:GetService(serviceName: string)
 	
 	assert(serviceName, "Argument #1 missing or nil");
 	assert(typeof(serviceName) == "string", 'Invalid argument #1 to "GetService" (string expected, got ' .. typeof(serviceName) .. ')');
 	
-	if serviceName:lower() == "cache" then return luna_storage; end;
+	if serviceName:lower() == "cache" then return luna_cache; end;
 	if luna_xyz_env.loaded_libs[serviceName] then return luna_xyz_env.loaded_libs[serviceName]; end;
 	
 	local service = cloneref(game:GetService(serviceName));
@@ -21,7 +24,7 @@ end;
 function Services:GetServices(serviceNames: table)
 	
 	assert(serviceNames, "Argument #1 missing or nil");
-	assert(typeof(serviceNames) == "table", 'Invalid argument #1 to "GetServices" (string expected, got ' .. typeof(serviceName) .. ')');
+	assert(typeof(serviceNames) == "table", 'Invalid argument #1 to "GetServices" (string expected, got ' .. typeof(serviceNames) .. ')');
 	
 	for _, serviceName: string in ipairs(serviceNames) do
 		Services:GetService(serviceName);
