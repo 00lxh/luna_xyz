21666084ae309f48fa8f3f64a7b921cfa712c514
00._lxh <lxcy.armendarizz@gmail.com>
2026-04-12 14:32:23 -0600

    Update Notify.lua

--- src/utils/ui/Notify.lua
+++ src/utils/ui/Notify.lua
@@ -1,24 +1,48 @@
-local Notifications = {};
-local CacheFoler = Services:GetService("Cache");
+local Notifications = {
+	NotifyVolume = 2; NotifySound = true; NotifySoundId = 82845990304289; DefaultSoundId = 82845990304289;
+};
+
+local CacheFoler = luna_xyz_env:GetService("Cache");
 
 function Notifications:Notify(str: string, time: number)
-	
-	if luna_xyz_env.NotifySound then
-		
+
+	if Notifications.NotifySound then
+
 		local notifySound = Instance.new("Sound", CacheFoler);
 		notifySound.PlayOnRemove = true;
-		
-		notifySound.Volume = luna_xyz_env.NotifyVolume;
-		notifySound.SoundId = string.format("rbxassetid://%d", luna_xyz_env.NotifySoundID);
+
+		notifySound.Volume = Notifications.NotifyVolume;
+		notifySound.SoundId = string.format("rbxassetid://%d", Notifications.NotifySoundId);
 
 		notifySound:Destroy();
 	end;
-	
+
 	luna_xyz_env.Library:Notify({
 
 		Icon = "rbxassetid://5012126105"; Time = time or 5;
 		Title = '<b><font size="20">luna.xyz - ' .. tostring(luna_xyz_env.versions["luna_xyz_loader"]) .. '</font></b>'; Description = tostring(str);
 	});
+end;
+
+function Notifications:GetCustomNotifications()
+	
+	return {};
+end;
+
+function Notifications:SetVolume(volume: number)
+	
+	assert(luna_xyz_env:IsNumber(volume), "Invalid Volume number.");
+	Notifications.NotifyVolume = volume;
+end;
+
+function Notifications:SetSoundId(soundId: number)
+	
+	assert(luna_xyz_env:IsNumber(soundId), "Invalid Volume number.");
+	Notifications.NotifySoundId = soundId;
+end;
+
+function Notifications:ToggleSound(value: boolean)
+	Notifications.NotifySound = luna_xyz_env:ParseBoolean(value);
 end;
 
 return Notifications;
