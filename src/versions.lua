dcd955cf0dc0895016da6d7aaac3edd95c9b031c
00._lxh <lxcy.armendarizz@gmail.com>
2026-04-19 13:45:31 -0600

    Update versions.lua

--- src/versions.lua
+++ src/versions.lua
@@ -1,10 +1,12 @@
 local versions = {
 
 	["luna_xyz_loader"] = "v7.2.5";
-	["rakoof"] = "v3.2.1";
-
+	
+	["the_rakoof"] = "v3.2.1";
 	["rake_remastered"] = "v2.2.1";
+	
 	["revenant_sunrisen"] = "v2.3.2";
+	["project_lazarus"] = "v1.0.0";
 };
 
 return versions;
