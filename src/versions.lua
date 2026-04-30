e1b7458d73d8f3fc5ec955f59620f867bc36777c
00._lxh <lxcy.armendarizz@gmail.com>
2026-04-30 17:21:17 -0600

    Update versions.lua

--- src/versions.lua
+++ src/versions.lua
@@ -1,12 +1,12 @@
 local versions = {
 
-	["luna_xyz_loader"] = "v7.2.6";
+	["luna_xyz_loader"] = "v7.3.0";
 	
 	["the_rakoof"] = "v3.2.1";
 	["rake_remastered"] = "v2.2.2";
 	
-	["revenant_sunrisen"] = "v2.3.2";
 	["project_lazarus"] = "v1.0.0";
+	["criminality"] = "v1.0.0";
 };
 
 return versions;
