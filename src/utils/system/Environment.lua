78d33ccf78cd7b520bda09b90e280af8953a91ee
00._lxh <lxcy.armendarizz@gmail.com>
2026-04-30 16:16:14 -0600

    Update Environment.lua

--- src/utils/system/Environment.lua
+++ src/utils/system/Environment.lua
@@ -46,23 +46,13 @@ end;
 function luna_xyz_env:GetService(serviceName: string)
 
 	assert(serviceName, "Argument #1 missing or nil");
-	assert(typeof(serviceName) == "string", 'Invalid argument #1 to "GetService" (string expected, got ' .. typeof(serviceName) .. ')');
+	assert(typeof(serviceName) == "string", ('Invalid argument #1 to "GetService" (string expected, got %s)'):format(typeof(serviceName)));
 
 	if serviceName:lower() == "cache" then return luna_cache; end;
 	if luna_xyz_env.loaded_libs[serviceName] then return luna_xyz_env.loaded_libs[serviceName]; end;
 
 	local service = cloneref(game:GetService(serviceName));
 	return service;
-end;
-
-function luna_xyz_env:GetServices(serviceNames: table)
-
-	assert(serviceNames, "Argument #1 missing or nil");
-	assert(typeof(serviceNames) == "table", 'Invalid argument #1 to "GetServices" (table expected, got ' .. typeof(serviceNames) .. ')');
-
-	for _, serviceName: string in ipairs(serviceNames) do
-		luna_xyz_env:GetService(serviceName);
-	end;
 end;
 
 function luna_xyz_env:GetMousePosition()
