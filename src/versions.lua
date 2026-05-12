385b1059de4ba8cf1950827911897ac92a6a01f1
00._lxh <lxcy.armendarizz@gmail.com>
2026-05-11 19:01:52 -0600

    Update versions.lua

--- src/versions.lua
+++ src/versions.lua
@@ -5,8 +5,8 @@ local versions = {
 	["the_rakoof"] = "v3.2.1";
 	["rake_remastered"] = "v2.2.2";
 	
-	["project_lazarus"] = "v1.0.0";
-	["criminality"] = "v1.0.0";
+	["project_lazarus"] = "v1.0.1";
+	["99_nights_in_the_forest"] = "v1.0.0";
 };
 
 return versions;
