8a032c126170ed0db035e3ab8fb448964fcd8411
00._lxh <lxcy.armendarizz@gmail.com>
2026-06-06 14:59:56 -0600

    Update versions.lua

--- src/versions.lua
+++ src/versions.lua
@@ -1,11 +1,11 @@
 local versions = {
 
-	["luna_xyz_loader"] = "v7.3.1";
+	["luna_xyz_loader"] = "v7.3.2";
 	
-	["the_rakoof"] = "v3.2.1";
+	["the_rakoof"] = "v3.2.2";
 	["rake_remastered"] = "v2.2.3";
 	
-	["project_lazarus"] = "v1.0.1";
+	["project_lazarus"] = "v1.0.2";
 	["violence_district"] = "v1.0.0";
 };
 
