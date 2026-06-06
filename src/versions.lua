937203549ad76a563033111b32ef366b6b90caf3
00._lxh <lxcy.armendarizz@gmail.com>
2026-06-06 14:59:18 -0600

    Update versions.lua

--- src/versions.lua
+++ src/versions.lua
@@ -3,10 +3,10 @@ local versions = {
 	["luna_xyz_loader"] = "v7.3.1";
 	
 	["the_rakoof"] = "v3.2.1";
-	["rake_remastered"] = "v2.2.2";
+	["rake_remastered"] = "v2.2.3";
 	
 	["project_lazarus"] = "v1.0.1";
-	["99_nights_in_the_forest"] = "v1.0.0";
+	["violence_district"] = "v1.0.0";
 };
 
 return versions;
